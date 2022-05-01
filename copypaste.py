2b4ba412016971fb72f9768606ac4f8c7dcb1be5
Spencer Gibson <spencergibson26@gmail.com>
2022-05-01 17:13:18 -0400

    Update copypaste.py
    Previously, I had different function for pasting the different entries in the clipboard list.
    
    However, we can abstract away from that using the partial function from the Python functools library.
    
    This code is much cleaner and scalable.

--- copypaste.py
+++ copypaste.py
@@ -2,6 +2,7 @@ from pynput import keyboard
 import sys
 from pyperclip import copy, paste
 from time import sleep
+from functools import partial
 
 class React_to_Keyboard():
     def __init__(self):
@@ -10,30 +11,19 @@ class React_to_Keyboard():
         self.prev_copied = paste()
 
     def on_activate_copy(self):
-        sleep(.01)
-        (self.curr_queue, self.prev_copied) = add_to_queue(self.curr_queue, self.max_len, self.prev_copied)
+        sleep(.1)
+        (self.curr_queue, self.prev_copied) = add_to_queue(self.curr_queue,
+        self.max_len, self.prev_copied)
 
-    def on_activate_paste_1(self):
-        paste_from_queue(self.curr_queue, 0)
-
-    def on_activate_paste_2(self):
-        paste_from_queue(self.curr_queue, 1)
-
-    def on_activate_paste_3(self):
-        paste_from_queue(self.curr_queue, 2)
-
-    def on_activate_paste_4(self):
-        paste_from_queue(self.curr_queue, 3)
-
-    def on_activate_paste_5(self):
-        paste_from_queue(self.curr_queue, 4)
+    def on_activate_paste(self, num):
+        paste_from_queue(self.curr_queue, num)
 
     def on_release(self):
         self.curr_queue = []
         raise sys.exit(1)
 
 ###Rough outline -- You can keep up to 5 things copied.
-###If you add sometThe Death of Democracy: Hitler's Rise to Power and the Downfall of the Weimar Republicclipboard, then you should add it to the front
+###If you add something to the clipboard, then you should add it to the front
 ###Of the queue. If there are too many items in the queue, remove the last item
 ###Before adding the new copied text
 def add_to_queue(curr_queue, max_len, prev_copied):
@@ -47,7 +37,6 @@ def add_to_queue(curr_queue, max_len, prev_copied):
     return (curr_queue, copied_Text)
 
 ###This function pastes what you copied in a certain position
-###It will use pandas to do this
 def paste_from_queue(curr_queue, pos):
     ###Check that the position is valid
     if pos < 0 or pos >= len(curr_queue):
@@ -61,11 +50,11 @@ def main():
     react = React_to_Keyboard()
     with keyboard.GlobalHotKeys({
             '<cmd>+c': react.on_activate_copy,
-            '<ctrl>+v+q': react.on_activate_paste_1,
-            '<ctrl>+v+w': react.on_activate_paste_2,
-            '<ctrl>+v+e': react.on_activate_paste_3,
-            '<ctrl>+v+r': react.on_activate_paste_4,
-            '<ctrl>+v+t': react.on_activate_paste_5,
+            '<ctrl>+v+q': partial(react.on_activate_paste, 0),
+            '<ctrl>+v+w': partial(react.on_activate_paste, 1),
+            '<ctrl>+v+e': partial(react.on_activate_paste, 2),
+            '<ctrl>+v+r': partial(react.on_activate_paste, 3),
+            '<ctrl>+v+t': partial(react.on_activate_paste, 4),
             '<ctrl>+j': react.on_release}) as h:
         h.join()
 
